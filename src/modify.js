270489e703083ac87e07b397ae13672c226a423f
Marcy <madisontolentino@gmail.com>
2024-05-21 15:59:44 -0400

    unfinished

--- src/modify.js
+++ src/modify.js
@@ -1,8 +1,19 @@
 // These are your event handlers
 const clickCounterHandler = () => {
+  const button = document.querySelector('#click-button')
+  button.addEventListener('click', () => {
+    const counter = Number(button.target.textContent)
+    button.textContent = counter++
+    button.click.append(counter);
+  })
+
 };
 
 const handleKeydown = () => {
+  const tracker = document.querySelector('#keydown-tracker')
+  tracker.addEventListener('click', () => {
+    tracker.textContent = `You pressed ${tracker.key}`
+  })
 };
 
 // We've started this one for you
