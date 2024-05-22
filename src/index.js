89fb403a91b72f005cc93bd68184740f40ffc233
Marcy <madisontolentino@gmail.com>
2024-05-22 13:38:04 -0400

    ITZEL I DID IT OMGGGGG

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const incButton = document.querySelector('#add-one')
+
+const incFunc = () => {
+  let p = document.querySelector('#results')
+  let num = Number(document.querySelector('#results').textContent)
+  num++
+  p.textContent = num
+}
+
+incButton.addEventListener('click', incFunc)
--- src/modify.js
+++ src/modify.js
@@ -1,28 +1,66 @@
-// These are your event handlers
+// These are your event handlers 
+const button = document.getElementById('click-button')
 const clickCounterHandler = () => {
-  const button = document.querySelector('#click-button')
-  button.addEventListener('click', () => {
-    const counter = Number(button.target.textContent)
-    button.textContent = counter++
-    button.click.append(counter);
-  })
+ 
+  let counter = Number(button.getAttribute('data-clicks'))
+  counter++
 
+  button.setAttribute('data-clicks', counter)
+
+  if (counter === 1){
+    button.textContent = `I've been clicked ${counter} time.`
+  } else {
+  button.textContent = `I've been clicked ${counter} times!`
+  }
 };
 
-const handleKeydown = () => {
+const button2 = document.getElementById('inline-example')
+const clickCounterHandlerInline = () => {
+ 
+  let counter = Number(button2.getAttribute('data-clicks'))
+  counter++
+
+  button2.setAttribute('data-clicks', counter)
+
+  if (counter === 1){
+    button2.textContent = `I've been clicked ${counter} time.`
+  } else {
+  button2.textContent = `I've been clicked ${counter} times!`
+  }
+};
+
+const handleKeydown = (event) => {
   const tracker = document.querySelector('#keydown-tracker')
-  tracker.addEventListener('click', () => {
-    tracker.textContent = `You pressed ${tracker.key}`
-  })
+  let key =  event.code
+  tracker.textContent = `You pressed ${key}`
+  
 };
 
 // We've started this one for you
 const handleDelegation = (event) => {
   const resultSpan = document.querySelector('#delegation-result');
-  resultSpan.textContent = event.target.textContent;
+  if(event.target.matches('#up') || event.target.matches('#middle') ||event.target.matches('#down') || event.target.matches('#left') || event.target.matches('#right')){
+      resultSpan.textContent = event.target.textContent;
+  } else {
+    console.log('not a button')
+  }
 };
 
+const removeButton = document.querySelector('#remove')
+
+const removeDelListener = () => {
+  delegation.removeEventListener('click', handleDelegation)
+}
+
+
 const addNewRandomNumber = () => {
+  const randomNum = document.querySelector('#add-random-num')
+  const randomNumUL = document.querySelector('#random-numbers')
+
+  const li = document.createElement('li')
+  const randomNumberGenerator = Math.floor(Math.random() * 100)
+  li.innerText = randomNumberGenerator
+  randomNumUL.append(li)
 };
 
 
@@ -31,7 +69,17 @@ const main = () => {
   const delegationContainer = document.querySelector('#delegation');
   delegationContainer.addEventListener('click', handleDelegation);
 
+  const buttonContainer = document.querySelector('#click-button')
+  buttonContainer.addEventListener('click', clickCounterHandler)
+
+  const buttonContainer2 = document.querySelector('#inline-example')
+  buttonContainer2.addEventListener('click', clickCounterHandlerInline)
   
+  document.body.addEventListener('keydown', handleKeydown)
+
+  removeButton.addEventListener('click', removeDelListener)
+
 };
 
 main();
+
